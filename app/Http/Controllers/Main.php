f6670d56d8d461f2f8922417624062d8a62146ad
JadsonJose <jadsonjosesilva@gmail.com>
2020-12-15 21:22:58 -0300

    sinc

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -15,15 +15,38 @@ class Main extends Controller
     // =====================================================
     public function init(Request $request)
     {
+        // check if there was post
+        if (!$request->isMethod('post')) {
+            return redirect()->route('main_index');
+        }
+
+        // validation
+        $request->validate(
+            [
+                'text_from' => 'required|email|max:50',
+                'text_to'  => 'required|email|max:50',
+                'text_message' => 'required|max:250'
+            ],
+            [
+                'text_from.required' => 'From is required',
+                'text_from.email' => 'From must be a valid email',
+                'text_from.max' => 'From must have less than 50 chars',
+                'text_to.required' => 'To is required',
+                'text_to.email' => 'To must be valid email',
+                'text_to.max' => 'To must have less than 50 chars',
+                'text_message.required' => 'Message is required',
+                'text_message.max' => 'Message must have less than 250 chars'
+            ]
+        );
     }
 
     // =====================================================
-    public function confirm($purl)
-    {
-    }
+    // public function confirm($purl)
+    // {
+    // }
 
-    // =====================================================
-    public function read($purl)
-    {
-    }
+    // // =====================================================
+    // public function read($purl)
+    // {
+    // }
 }
--- resources/views/message_form.blade.php
+++ resources/views/message_form.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.app_layout')
 
 @section('content')
-    AQUIII!!!!!
+
+
+    <div class="row">
+        <div class="col-sm-4 offset-sm-4">
+
+            @csrf
+
+            <form action="{{ route('main_init') }}" method="post">
+                <div class="form-group">
+                    <label>From:</label>
+                    <input type="email" name="text_from" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>To:</label>
+                    <input type="email" name="text_to" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Message:</label>
+                    <textarea name="text_message" rows="5" class="form-control"></textarea>
+                </div>
+
+                <div class="text-center mt-3">
+                    <input type="submit" value="Send One Time Message" class="btn btn-primary">
+                </div>
+
+
+            </form>
+
+            @if ($errors->any())
+                <div class="alert alert-danger p-2">
+                    <ul>
+                        @foreach ($errors->all as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </div>
+
 @endsection
